c72060bd65c5d2f6bd709a06ea2effdd8aa36f5e
haythmelmagraby <haythmelmagraby@gmail.com>
2024-03-24 14:35:40 -0700

    5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,7 +2,7 @@
 """Storage Module"""
 import json
 import os
-from models.base_model import BaseModel
+import datetime
 
 class FileStorage:
     """ file storage class """
@@ -12,7 +12,8 @@ class FileStorage:
     def new(self, obj):
         """sets in __objects the obj"""
         obj_cn = obj.__class__.__name__
-        Filestorage["{}.{}".format(obj_cn, obj.id)] = obj
+        the_key = "{}.{}".format(obj_cn, obj.id)
+        FileStorage.__objects[the_key] = obj
 
     def all(self):
         """the dictionary __objects"""
@@ -20,17 +21,17 @@ class FileStorage:
 
     def save(save):
         """serializes __objects to the JSON file"""
-        objs = Filestorage.__objects
+        objs = FileStorage.__objects
         my_dict = {}
-        for obj in objs.key:
-            my_dict[key] = objs[obj].to_dict()
+        for obj in objs.keys():
+            my_dict[obj] = objs[obj].to_dict()
 
         with open(FileStorage.__file_path, "w",encoding="utf-8") as f:
             json.dump(my_dict, f)
 
     def reload(self):
         """deserializes the JSON file to __objects"""
-        if os.path.isfile(Filestorage.__file_path):
+        if os.path.isfile(FileStorage.__file_path):
             with open (File_Storage.__file_path, "r", encoding="utf-8") as f:
                     my_dict = json.load(f)
                     for key, value in my_dict.items():
