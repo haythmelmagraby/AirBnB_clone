3923bb9da5e93457eda60ede3df184d7e875707e
haythmelmagraby <haythmelmagraby@gmail.com>
2024-03-25 03:15:38 -0700

    7

--- console.py
+++ console.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """console module"""
 import cmd
+from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
     """HBNBCommand consol class"""
     prompt = "(hbnb)"
+    classes = {"BaseModel":BaseModel}
 
     def emptyline(self):
         """dont execute anything"""
@@ -17,6 +20,46 @@ class HBNBCommand(cmd.Cmd):
 
     def do_quit(self, line):
         'Quit command to exit the program'
+        return True
+
+    def do_create(self, line):
+        'Creates a new instance of BaseModel'
+        if line not in self.classes:
+            print("** class doesn't exist **")
+        elif line is None or line == "":
+            print("** class name missing **")
+        elif line in self.classes:
+            new_ins = self.classes[line]()
+            new_ins.save()
+            print(new_ins.id)
+
+    def do_show(self, line):
+        'Prints the string'
+        if line is None or line == "":
+            print("** class name missing **")
+        else:
+            splitted = line.split(' ')
+            if splitted[0] not in self.classes:
+                print("** class doesn't exist **")
+            elif len(splitted) < 2:
+                print("** instance id missing **")
+            else:
+                result = "{}.{}".format(splitted[0], splitted[1])
+                if result in storage.all():
+                    print(storage.all()[result])
+                else:
+                    print("** no instance found **")
+
+    def do_destroy(self, line):
+        'Deletes an instance based on the class name'
+        return True
+
+    def do_all(self, line):
+        'Prints all string representation of all instances'
+        return True
+
+    def do_update(self, line):
+        'Updates an instance based on the class name'
         return True
 
 
